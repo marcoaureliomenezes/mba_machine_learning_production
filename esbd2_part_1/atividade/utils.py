0a32e0acc09b1072a9d4794ccab1f8063d2d3d93
Marco Menezes <marcoaurelioreislima@gmail.com>
2023-05-16 19:48:53 -0400

    updating some aspects of esbd2_part_1

--- esbd2_part_1/atividade/utils.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-
-
-def plot_bidirectional_graph(adjacent_list):
-    G = nx.DiGraph(adjacent_list)
-    nx.draw(G, with_labels=True)
-    plt.show()
